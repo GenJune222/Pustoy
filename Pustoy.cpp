e347cc97042a856baf7344799f40852ff2e097fc
GenJune <julia.nepochatova@gmail.com>
2025-11-10 22:16:34 +0300

    vrode pyatiy commit

--- Pustoy.cpp
+++ Pustoy.cpp
@@ -5,27 +5,34 @@ struct IntArray {
     int get(size_t id) const;
     size_t size() const;
     int last() const;
+    IntArray(int i);
+    ~IntArray();
 };
 
 int main()
 {
     int next = 0;
     std::cin >> next;
-    IntArray a;
-    a.add(next);
-    while (std::cin >> next)
-    {
-        a.add(next);
-    }
-    if (std::cin.fail())
-    {
-        return 1;
-    }
 
-    size_t count = 1;
-    for (size_t i = 0; i < a.size() - 1; ++i)
-    {
-        int d = a.get(i);
-        count += !(d % a.last());
+    try {
+        IntArray a(next);
+        while (std::cin >> next)
+        {
+            a.add(next);
+        }
+        if (std::cin.fail() && !std::cin.eof())
+        {
+            return 1;
+        }
+        size_t count = 1;
+        for (size_t i = 0; i < a.size() - 1; ++i)
+        {
+            int d = a.get(i);
+            count += !(d % a.last());
+        }
+        std::cout << count << "\n";
+
+        catch (const std::bad_alloc) {
+            return 2;
+        }
     }
-}
